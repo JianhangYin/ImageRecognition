f8d613f5274cea6dbbf75fbf2a9a427b0629ab70
Jianhang Yin <yinjianhang@ou.edu>
2019-03-04 22:47:55 +0800

    add basename to BrowerRouter

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Main from './containers/Main';
 
 export default (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Route exact path={'/'} component={Main}/>
   </Router>
 );
